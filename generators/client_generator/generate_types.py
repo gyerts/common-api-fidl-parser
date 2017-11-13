b1b7146911b8594c5123a4b71883fc9e5bb77238
Yuriy Gyerts (BP10534) <YGyerts@luxoft.com>
2017-11-13 14:38:31 +0200

    add types int8-16-32

--- generators/client_generator/generate_types.py
+++ generators/client_generator/generate_types.py
@@ -1,10 +1,15 @@
 def _replace(param: dict, namespace: str):
     if param["type"] == "String":
         param["type"] = "std::string"
-    elif param["type"] == "Int":
-        param["type"] = "int"
+    elif param["type"] == "Int32":
+        param["type"] = "int32_t"
+    elif param["type"] == "Int16":
+        param["type"] = "int16_t"
+    elif param["type"] == "Int8":
+        param["type"] = "int8_t"
     else:
         param["type"] = "{}::{}".format(namespace, param["type"])
+
 
 def generate_types(method: dict, namespace: str):
     if "in" in method:
