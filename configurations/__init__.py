6804d1dbe4f39f950ae7dab370d796b8898abbf1
Yuriy Gyerts (BP10534) <YGyerts@luxoft.com>
2017-11-16 14:08:19 +0200

    [CALLBACK] add callback option

--- configurations/__init__.py
+++ configurations/__init__.py
@@ -2,5 +2,6 @@ settings = {
     "company": "Luxoft",
     "project_name": "UserManager",
     "author": "Yuriy Gyerts",
-    "is_mock": True
+    "is_mock": True,
+    "generate_callbacks": True
 }
